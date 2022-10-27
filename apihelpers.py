1f9678ac85c60ee4af80e7fdd63ca19fea2f311c
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-10-27 16:34:21 -0600

    Almost finished the warmup

---
+++ apihelpers.py
@@ -0,0 +1,21 @@
+# function responsible to the sent_data that will is going to be request.args or request.json and the
+# expected_data taht is going to be the list of keys the endpoint requires
+# this function will return a string in case of error and None otherwise
+
+
+def check_endpoint_info(sent_data, expected_data):
+    for data in expected_data:
+        if (sent_data.get(data) == None):
+            return f"The {data} argument is required."
+
+
+def check_data_sent(sent_data, expected_data, user_info):
+    user = {}
+    i = 0
+    for data in expected_data:
+        if (sent_data.get(data) == None):
+            user[data] = user_info[i]
+        else:
+            user[data] = sent_data[data]
+        i += 1
+    return user
---
+++ app.py
@@ -0,0 +1,39 @@
+import secrets
+from flask import Flask, request, make_response
+from dbhelpers import run_statement
+from apihelpers import check_endpoint_info, check_data_sent
+from dbcreds import production_mode
+import json
+
+app = Flask(__name__)
+
+@app.post('/api/client')
+def post_client():
+    client_info = run_statement('CALL get(?)', [
+                                request.headers.get('token')])
+
+    update_info_client = check_data_sent(request.json, [
+        'email', 'first_name', 'last_name', 'image_url', 'username', 'password'], client_info[0])
+
+    results = run_statement('CALL update_info_client(?,?,?,?,?)',
+                            [update_info_client['email'], update_info_client['password'],
+                             update_info_client['bio_input'], update_info_client['image_url'], request.headers.get('token')])
+
+    if (type(results) == list and results[0][0] == 1):
+        return make_response(json.dumps(results[0][0], default=str), 200)
+    elif (type(results) == list and results[0][0] == 0):
+        return make_response(json.dumps("Bad request."), 400)
+    else:
+        return make_response(json.dumps("Sorry, an error has occurred.", default=str), 500)
+
+##############################################################################################################
+
+if (production_mode):
+    print("Running in Production Mode")
+    import bjoern  # type: ignore
+    bjoern.run(app, "0.0.0.0", 5000)
+else:
+    from flask_cors import CORS
+    CORS(app)
+    print("Running in Testing Mode")
+    app.run(debug=True)
